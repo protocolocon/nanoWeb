0bd6d528abbefa4feb27212ef622cb77c49d1b09
proto <protocolocon@gmail.com>
2016-10-25 20:47:30 +0200

    Hello world! example

---
+++ example/hello_world/application.ml
@@ -0,0 +1,11 @@
+// Example application description
+Application {
+    color: "#4d4d4d"
+    
+    Button {
+        id: buttonTest
+        width: 320
+        height: 100
+        color: "#ffc000"
+    }
+}
